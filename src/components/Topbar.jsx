425c52dbe44d7d391a4aeb9a689a4b6d17fb53db
JacekGos <jacekg93@gmail.com>
2023-04-04 20:10:24 +0200

    Updated Topbar content and styles

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Navbar, Nav, Form } from "react-bootstrap";
+import { Container, Navbar, Nav, Form, Button } from "react-bootstrap";
 
 
 export default function Topbar() {
@@ -7,24 +7,35 @@ export default function Topbar() {
     return (
         <Navbar collapseOnSelect expand="lg" variant="dark">
             <Container>
-                <Navbar.Brand href="/">
-                    <h3>Team-finder.pl</h3>
+                <Navbar.Brand className="mb-0" href="/">
+                    <p className="brand-title">Team-finder.pl</p>
                 </Navbar.Brand>
+                <Nav className="me-auto">
+                    <Form>
+                        <Form.Check
+                            type="switch"
+                            id="custom-switch"
+                        />
+                    </Form>
+                </Nav>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Form>
-                            <Form.Check
-                                type="switch"
-                                id="custom-switch"
-                            />
-                        </Form>
                     </Nav>
-                    <Nav className="justify-content-center">
-                        <Nav.Link href="#option1">Więcej</Nav.Link>
-                        <Nav.Link eventKey={1} href="#option2">
-                            Opcje
-                        </Nav.Link>
+                    <Nav >
+                        <Nav.Item>
+                            <Nav.Link eventKey={1} href="#option1">
+                                Wydarzenia
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+
+                            <Nav.Link eventKey={2} href="#option" >
+                                <Button className="btn-topbar-signin">
+                                    Zaloguj
+                                </Button>
+                            </Nav.Link>
+                        </Nav.Item>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
