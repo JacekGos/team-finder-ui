cb2a6ef49fbaa829c51f8b04013f1aa16f90eedc
JacekGos <jacekg93@gmail.com>
2023-04-07 14:50:46 +0200

    Added more filters button

--- src/components/Filterbar.jsx
+++ src/components/Filterbar.jsx
@@ -1,23 +1,29 @@
 import React from "react";
-import { Container, Row, Col, Form } from "react-bootstrap";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import {BsSliders2} from "react-icons/bs";
 
 export default function Topbar() {
 
     return (
-        <Container style={{position: "sticky"}}>
+        <Container style={{ position: "sticky" }}>
             <Row>
-                <Col sm={2} style={{display: 'flex', alignItems: 'center', justifyContent: "left"}} >
+                <Col sm={2} style={{ display: 'flex', alignItems: 'center', justifyContent: "left" }} >
                     <Form>
                         <Form.Group>
-                            <Form.Control placeholder="lokalizacja" style={{ color: 'gray', height: 35, borderRadius: 30}}/>
+                            <Form.Control placeholder="lokalizacja" style={{ color: 'gray', height: 35, borderRadius: 30 }} />
                         </Form.Group>
                     </Form>
                 </Col>
-                <Col sm={8} style={{backgroundColor: 'red', height: 70, color: 'white'}}>
+                <Col sm={8} style={{ backgroundColor: 'red', height: 70, color: 'white' }}>
                     DISCIPLINES ICONS
                 </Col>
-                <Col sm={2} style={{backgroundColor: 'blue', color: 'white'}}>
-                    MORE
+                <Col sm={2} style={{ display: 'flex', alignItems: 'center', justifyContent: "right" }}>
+                    <Button className="btn-base" style={{display: 'flex', alignItems: 'center'}}>
+                        <BsSliders2  style={{marginRight: 5}}/>
+                        Więcej filtrów
+                    </Button>
+
+                    
                 </Col>
             </Row>
         </Container>
