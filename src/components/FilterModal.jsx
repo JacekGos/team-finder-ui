2a898fc8fe911797dd24373c8afbbe18c4d12a2b
JacekGos <jacekg93@gmail.com>
2023-04-08 16:31:01 +0200

    Added in mobile view modal, footer with confirm button

--- src/components/FilterModal.jsx
+++ src/components/FilterModal.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Modal, Button, Form } from "react-bootstrap";
+import { Modal, Button, Form, Container } from "react-bootstrap";
 import { TfiClose } from "react-icons/tfi";
 import { BiFootball, BiTennisBall } from "react-icons/bi";
 import { FaVolleyballBall, FaBasketballBall } from "react-icons/fa";
@@ -11,7 +11,7 @@ export default function FilterModal(props) {
         <Modal show={props.show} fullscreen={props.fullscreen}
             style={{ zIndex: 1100 }}
         >
-            <Modal.Header className="modal-base">
+            <Modal.Header className="modal-base" style={{ borderBottom: '1px solid #103861' }}>
                 <Modal.Title>Wyszukaj</Modal.Title>
                 <Button className="input-base rounded-circle d-flex align-items-center p-2" onClick={() => props.handleClose(false)}>
                     <TfiClose />
@@ -19,11 +19,11 @@ export default function FilterModal(props) {
             </Modal.Header >
             <Modal.Body className="modal-base p-0">
                 <Form>
-                    <Form.Group className="position-fixed d-flex align-items-center w-100" style={{ padding: '8px 30px 1px 10px',  backgroundColor: '#0A1929'}}>
+                    <Form.Group className="position-fixed d-flex align-items-center w-100" style={{ padding: '8px 30px 1px 10px', backgroundColor: '#0A1929' }}>
                         <Form.Control placeholder="lokalizacja" className="input-base input-search mb-3" />
                     </Form.Group>
                     <Form.Group className="d-flex flex-column align-items-start gap-3 ps-2">
-                        <div className="d-flex align-items-center" style={{marginTop: 70}}>
+                        <div className="d-flex align-items-center" style={{ marginTop: 70 }}>
                             <Button className="input-base rounded-circle" >
                                 <h2><BiFootball style={{ color: "white" }} className="p-0 m-0" /></h2>
                             </Button>
@@ -84,6 +84,12 @@ export default function FilterModal(props) {
                             <p className="p-0 m-0 ms-3 fs-5">Rower</p>
                         </div>
                     </Form.Group>
+                    <Container className="position-fixed position-absolute bottom-0 end-0 d-flex align-items-center justify-content-center w-100"
+                        style={{ padding: '5px 0px 5px 10px', backgroundColor: '#0A1929' , height: 70, borderTop: '1px solid #103861'}}>
+                        <Button className="input-base ">
+                            Pokaż wydażenia
+                        </Button>
+                    </Container>
                 </Form>
             </Modal.Body>
         </Modal>
