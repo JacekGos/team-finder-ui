cef18d5c30319a5be9c08c5b3da967b6f2865d8b
JacekGos <jacekg93@gmail.com>
2023-04-09 17:54:36 +0200

    Added EventTag items in mobile view event tab

---
+++ src/components/EventTab.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import '../styles/events.css';
+import { Container, Row, Col, Badge } from "react-bootstrap";
+import { BiFootball } from "react-icons/bi";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+
+
+export default function EventTab(props) {
+
+    return (
+        <Container fluid className="mt-1" style={{ height: 80, backgroundColor: '#0E2237', borderRadius: 5 }}>
+            <Row className="h-100 p-1">
+                <Col xs={3} className="d-flex align-items-center justify-content-center rounded" style={{ backgroundColor: '#0A1929' }}>
+                    <BiFootball size={45} style={{ color: "white" }} />
+                </Col>
+                <Col xs={9} style={{ color: 'white' }}>
+                    <Row className="h-50">
+                        <Col className="d-flex align-items-center justify-content-start" style={{ fontSize: 15 }}>
+                            {props.name}
+                        </Col>
+                        <Col className="d-flex align-items-center justify-content-end" style={{ fontSize: 13 }}>
+                            <Badge bg='secondary'>
+                                {props.price} PLN
+                            </Badge>
+                        </Col>
+                    </Row>
+                    <Row className="h-50">
+                        <Col className="d-flex align-items-center justify-content-start" style={{ fontSize: 15 }}>
+                            {props.date}
+                        </Col>
+                        <Col className="d-flex align-items-center justify-content-end" style={{ fontSize: 13 }}>
+                            {props.location}
+                            <HiOutlineLocationMarker size={15} style={{ color: 'white', marginLeft: 3 }} />
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+
+        </Container>
+    )
+}
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,19 +1,93 @@
 import React from "react";
 import '../styles/events.css';
 import { Container, Row, Col, Tabs, Tab } from "react-bootstrap";
+import EventTab from "./EventTab";
 
 export default function Events() {
 
+    let events = [
+        {
+            name: 'Gierka',
+            price: 10,
+            date: '09.04 14:00',
+            location: 'Kielce, Krakowska 15'
+        },
+        {
+            name: 'Gierka',
+            price: 10,
+            date: '09.04 14:00',
+            location: 'Kielce, Krakowska 15'
+        },
+        {
+            name: 'Gierka',
+            price: 10,
+            date: '09.04 14:00',
+            location: 'Kielce, Krakowska 15'
+        },
+        {
+            name: 'Gierka',
+            price: 10,
+            date: '09.04 14:00',
+            location: 'Kielce, Krakowska 15'
+        },
+        {
+            name: 'Gierka',
+            price: 10,
+            date: '09.04 14:00',
+            location: 'Kielce, Krakowska 15'
+        },
+        {
+            name: 'Gierka',
+            price: 10,
+            date: '09.04 14:00',
+            location: 'Kielce, Krakowska 15'
+        },
+        {
+            name: 'Gierka',
+            price: 10,
+            date: '09.04 14:00',
+            location: 'Kielce, Krakowska 15'
+        },
+        {
+            name: 'Gierka',
+            price: 10,
+            date: '09.04 14:00',
+            location: 'Kielce, Krakowska 15'
+        },
+        {
+            name: 'Gierka',
+            price: 10,
+            date: '09.04 14:00',
+            location: 'Kielce, Krakowska 15'
+        },
+        {
+            name: 'Gierka',
+            price: 10,
+            date: '09.04 14:00',
+            location: 'Kielce, Krakowska 15'
+        },
+        {
+            name: 'Gierka',
+            price: 10,
+            date: '09.04 14:00',
+            location: 'Kielce, Krakowska 15'
+        },
+    ]
+
     return (
-        <Container fluid className="p-0">
+        <Container fluid className="p-0 d-inline d-lg-none">
             {/* MOBILE VIEW */}
-            <Tabs className="tabs d-flex d-lg-none">
-                <Tab eventKey="events" title="Wydażenia" className="tab" style={{ backgroundColor: 'red' }}>
+            <Tabs className="tabs ">
+                <Tab eventKey="events" title="Wydażenia">
+                    {events.map((data, key) => {
+                        return <EventTab key={key} name={data.name} price={data.price} date={data.date} location={data.location}/>
+                    })}
                 </Tab>
                 <Tab eventKey="map" title="Mapa">
                 </Tab>
             </Tabs>
-
+            
+            {/* DESKTOP VIEW */}
             <Row className="d-none d-lg-flex">
                 <Col>
                     COL 1
