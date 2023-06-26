8a81b1346e4a860a950554b027381a5622e1fdfd
JacekGos <jacekg93@gmail.com>
2023-06-26 18:27:01 +0200

    Updated build configs

--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ import App from './App';
 import "../node_modules/react-bootstrap/dist/react-bootstrap";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
     <App />
-  // {/* </React.StrictMode> */}
 );
 
